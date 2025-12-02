b9b56609e7cd2476cc060c2215be34ab1f43cab8
y893192 <yuanyan@stu.ouc.edu.cn>
2025-12-03 01:25:04 +0800

    update

--- adapteacher/__init__.py
+++ adapteacher/__init__.py
@@ -1,2 +1,3 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# --------------------------------------------------------------------------------------------------------------------
 from .config import add_ateacher_config
