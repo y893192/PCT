64a4c6bd141b9f3aa3aa01e76401418bdcec4264
y893192 <yuanyan@stu.ouc.edu.cn>
2025-12-03 01:26:18 +0800

    update

--- adapteacher/evaluation/__init__.py
+++ adapteacher/evaluation/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+# --------------------------------------------------------------------------------------------------------------------
 from .coco_evaluation import COCOEvaluator
 from .pascal_voc_evaluation import PascalVOCDetectionEvaluator
 
